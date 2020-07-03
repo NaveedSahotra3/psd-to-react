0e6ca3912b3b75b9ea3f563a31c32a144dfc1f13
Khuram Shahzad <>
2020-07-03 10:33:44 +0500

    little tweaks

--- psd-to-react/src/App.js
+++ psd-to-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import MainPage from './Component/MainPage/MainPage'
+
+
 function App() {
   return (
     <div className="App">
--- psd-to-react/src/Component/MainPage/MainPage.js
+++ psd-to-react/src/Component/MainPage/MainPage.js
@@ -63,7 +63,9 @@ class MainPage extends React.Component {
 
                     <div className="col-sm-6 heading">
 
-                        <h1 className="G-Gate">hjfk.kjfjgakjsdj</h1>
+                        <h1 className="G-Gate">
+                            <img src={require("./logo.jpg")} alt=""/>
+                        </h1>
 
                     </div>
 
@@ -148,7 +150,7 @@ class MainPage extends React.Component {
                             </div>
 
 
-                            <div className="col-md-12 img" > <img src={require('./helpbox-contact.jpg')} className="adjImg" />  </div>
+                            <div className="col-md-12 img" > <img src={require("./img1.jpeg")} alt="" className="adjImg" />  </div>
 
                             <div class='col-md-12 imgcard'>
 
@@ -205,42 +207,62 @@ class MainPage extends React.Component {
 
                                 <div className="row">
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color" >
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
                                     <div className="col-md-1 color">
-                                        <img src="" />
+                                        <img 
+                                        className="ourPartner-img"
+                                        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/green-company-logo-design-template-e6cda0adebc58ba974fa4d2d02a14d1e_screen.jpg?ts=1567170398" />
 
                                     </div>
 
